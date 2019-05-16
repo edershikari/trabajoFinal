999799add337de6b8c911dcad382970a96e44427
Eukene Padilla <tukeplayer@gmail.com>
2019-05-16 11:21:04 +0200

    añadir base de datos en formato sql al trabajo

--- src/controller/Carrito.java
+++ src/controller/Carrito.java
@@ -1,12 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.*;
 /**
  * Servlet implementation class Carrito
  */
@@ -26,6 +28,15 @@ public class Carrito extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Construir un String JSON con el arrayJava para enviar por AJAX
+//			ProductoModeloImp myModel= new ProductoModeloImp();
+//			String jsonString=JSONStringer.valueToString(myModel);
+//			PrintWriter out= response.getWriter();
+//			out.print(jsonString);
+//			out.flush();
+//		
+		
+		
 		request.getRequestDispatcher("view/vVercarrito.jsp").forward(request, response);
 	}
 
